15a703d0ccdff2d841805d41586907cc97542907
aisufiyanphp <115692677+aisufiyanphp@users.noreply.github.com>
2025-11-05 19:34:04 +0530

    admin panel setup
    with dashboard login page

---
+++ resources/views/admin/layout/footer.blade.php
@@ -0,0 +1,6 @@
+<footer class="main-footer">
+    <div class="float-right d-none d-sm-block">
+      <b>News CMS Version</b> 1
+    </div>
+    <strong>Copyright &copy; 2025-2026 <a href="https://archiveinfotech.com">Archive Infotech</a>.</strong> All rights reserved.
+</footer>
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get("admin/login", function(){
+  return view('admin.login');
+})->name('admin.login');
+Route::get("admin/forget-password", function(){
+  return view('admin.forget-password');
+})->name('admin.forget.password');
+
+Route::get('admin/dashboard', function(){
+  return view('admin.dashboard');
+})->name('admin.dashboard');
